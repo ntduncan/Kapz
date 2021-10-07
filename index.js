e82fbabee2b50f85213d66ef28cc7280d5d800d8
ntduncan <nateduncan0@.com>
2021-10-07 10:31:00 -0600

    added cors

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const products = require('./routes/productRoutes')
+
 const app = express();
 
 let urlencodedParser = bodyParser.urlencoded({extended: false})
+
+const corsOptions = {
+    origin: "https://kapz.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 //Connect to MongoDB
 const username = process.env.KAPZUSERNAME;
 const password = process.env.KAPZPASSWORD;
 
 const mongoCon = process.env.mongoCon || `mongodb+srv://${username}:${password}@cluster0.vctnn.mongodb.net/cluster0?retryWrites=true&w=majority`;
+
 mongoose.connect(mongoCon, {useNewUrlParser: true, useUnifiedTopology: true})
  .then(() => {
         console.log("Connected to MongoDB")
