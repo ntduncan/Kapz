46aabcb6e7263082fb972f5a71e9ad52264ebc77
ntduncan <nateduncan0@.com>
2021-10-07 10:52:40 -0600

    bleh bleh

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product")
-const productController = require("../controllers/ProductController");
+const productController = require("../controllers/productController");
 const bodyParser = require('body-parser')
 
 let urlencodedParser = bodyParser.urlencoded({extended: false})
